4bc73802dc5f44b57ccc95ed625d5027102509e4
junhaoyap <lordyyap@gmail.com>
2016-11-06 13:53:16 +0800

    Fix git get

--- backend/server/controllers/git.controller.js
+++ backend/server/controllers/git.controller.js
@@ -3,20 +3,20 @@ const cmd = require('node-cmd');
 
 function gitGet(req, res) {
   cmd.get(
-        `
-          cd git/scrapy
-          git ls-files | while read f; do git blame --line-porcelain $f | grep '^author '; done | sort -f | uniq -ic | sort -n
-        `,
-        function(data) {
-          var dataArr = data.split('\n');
-          for (var i = 0; i < dataArr.length; i++) {
-            dataArr[i] = dataArr[i].trim();
-          }
-          return res.json({
-            'res': dataArr
-          });
-        }
-    );
+    `
+      cd git/scrapy
+      git ls-files | while read f; do git blame --line-porcelain $f | grep '^author '; done | sort -f | uniq -ic | sort -n
+    `,
+    function(data) {
+      var dataArr = data.split('\n');
+      for (var i = 0; i < dataArr.length; i++) {
+        dataArr[i] = dataArr[i].trim();
+      }
+      return res.json({
+        'res': dataArr
+      });
+    }
+  );
 }
 
 function gitBlame(req, res) {
--- backend/server/routes/git.route.js
+++ backend/server/routes/git.route.js
@@ -7,7 +7,7 @@ import config from '../../config/env';
 
 const router = express.Router(); // eslint-disable-line new-cap
 
-router.route('/git-get').post(gitCtrl.gitGet);
+router.route('/git-get').get(gitCtrl.gitGet);
 
 router.route('/git-blame').get(gitCtrl.gitBlame);
 

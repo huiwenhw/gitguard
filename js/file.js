bbe0328be94f76bb188f1c6aa76a1d7c6b8873c2
junhaoyap <lordyyap@gmail.com>
2016-11-07 19:54:40 +0800

    Total working for specific file

--- js/file.js
+++ js/file.js
@@ -67,6 +67,9 @@
         $('#lines-id-partial-blame-div').removeClass('hidden');
         $('#lines-id-blame-div').addClass('hidden');
         $('#list-id').addClass('hidden');
+        $('#start-line').removeClass('hidden').val('');
+        $('#end-line').removeClass('hidden').val('');
+        $('#clear-blame').addClass('hidden');
       });
       $('#full-blame').click(function() {
         $('#partial-blame').addClass('btn-default').removeClass('btn-success');
@@ -75,6 +78,9 @@
         $('#lines-id-partial-blame-div').addClass('hidden');
         $('#lines-id-blame-div').removeClass('hidden');
         $('#list-id').addClass('hidden');
+        $('#start-line').addClass('hidden');
+        $('#end-line').addClass('hidden');
+        $('#clear-blame').addClass('hidden');
       });
       $('#history').click(function() {
         $('#partial-blame').addClass('btn-default').removeClass('btn-success');
@@ -83,6 +89,14 @@
         $('#lines-id-partial-blame-div').addClass('hidden');
         $('#lines-id-blame-div').addClass('hidden');
         $('#list-id').removeClass('hidden');
+        $('#start-line').addClass('hidden');
+        $('#end-line').addClass('hidden');
+        $('#clear-blame').addClass('hidden');
+      });
+      $('#clear-blame').click(function() {
+        $('#start-line').val('');
+        $('#end-line').val('');
+        triggerPartial();
       });
     }
 
@@ -95,14 +109,116 @@
       });
     }
 
+    var previousNumber = 0;
+
     function bindFile() {
       $('#lines-id-partial-blame').on('click', '.line-click', function() {
-        $('#lines-id-partial-blame-info p:nth-child(' + $(this).data('line') + ')').removeClass('hidden');
+        var lineNumber = parseInt($(this).data('line'));
+        if ($('#start-line').val() === '' && $('#end-line').val() === '') {
+          $('#start-line').val(lineNumber);
+          $('#end-line').val(lineNumber);
+          triggerPartial();
+          return;
+        } else {
+          var startNumber = parseInt($('#start-line').val());
+          var endNumber = parseInt($('#end-line').val());
+          if (lineNumber === startNumber || lineNumber === endNumber) {
+            return;
+          }
+          if (lineNumber < startNumber) {
+            $('#start-line').val(lineNumber);
+            triggerPartial();
+            previousNumber = parseInt(lineNumber);
+            return;
+          }
+          if (lineNumber > startNumber && lineNumber < endNumber) {
+            if (lineNumber < previousNumber) {
+              $('#start-line').val(lineNumber);
+              triggerPartial();
+              previousNumber = parseInt(lineNumber);
+              return;
+            }
+            if (lineNumber > previousNumber) {
+              $('#end-line').val(lineNumber);
+              triggerPartial();
+              previousNumber = parseInt(lineNumber);
+              return;
+            }
+          }
+          if (lineNumber > endNumber) {
+            $('#end-line').val(lineNumber);
+            triggerPartial();
+            previousNumber = parseInt(lineNumber);
+            return;
+          }
+        }
       });
     }
 
+    function triggerPartial() {
+      if ($('#start-line').val() !== '' && $('#end-line').val() !== '') {
+        for (var i = 0; i <= $('#lines-id-partial-blame-info').children().length; i++) {
+          $('#lines-id-partial-blame-info p:nth-child(' + i + ')').addClass('hidden');
+          $('#lines-id-partial-blame-info p:nth-child(' + i + ')').removeClass('text-invisible');
+        }
+
+        var startNumber = parseInt($('#start-line').val());
+        var endNumber = parseInt($('#end-line').val());
+
+        if (startNumber < 1) {
+          startNumber = 1;
+          $('#start-line').val(1);
+        }
+
+        if (startNumber > $('#lines-id-partial-blame-info').children().length + 1) {
+          startNumber = $('#lines-id-partial-blame-info').children().length + 1;
+          $('#start-line').val($('#lines-id-partial-blame-info').children().length + 1);
+          $('#end-line').val($('#lines-id-partial-blame-info').children().length + 1);
+        }
+
+        if (endNumber > $('#lines-id-partial-blame-info').children().length + 1) {
+          endNumber = $('#lines-id-partial-blame-info').children().length + 1;
+          $('#end-line').val($('#lines-id-partial-blame-info').children().length + 1);
+        }
+
+        for (var i = 0; i < startNumber; i++) {
+          $('#lines-id-partial-blame-info p:nth-child(' + i + ')').removeClass('hidden');
+          $('#lines-id-partial-blame-info p:nth-child(' + i + ')').addClass('text-invisible');
+        }
+
+        for (var i = startNumber; i <= endNumber; i++) {
+          $('#lines-id-partial-blame-info p:nth-child(' + i + ')').removeClass('hidden');
+        }
+      }
+      if ($('#start-line').val() === '' && $('#end-line').val() === '') {
+        for (var i = 0; i <= $('#lines-id-partial-blame-info').children().length; i++) {
+          $('#lines-id-partial-blame-info p:nth-child(' + i + ')').addClass('hidden');
+          $('#lines-id-partial-blame-info p:nth-child(' + i + ')').removeClass('text-invisible');
+        }
+      }
+      if ($('#start-line').val() === '' && $('#end-line').val() !== '') {
+        $('#start-line').val(parseInt($('#end-line').val()));
+        triggerPartial();
+        return;
+      }
+      if ($('#start-line').val() !== '' && $('#end-line').val() === '') {
+        $('#end-line').val(parseInt($('#start-line').val()));
+        triggerPartial();
+        return;
+      }
+    }
+
     function bindBox() {
-      // Bind a box for user to choose what line to what line to show blame
+      $('#start-line').keypress(function(event) {
+        var charCode = event.keyCode;
+        return !(charCode > 31 && (charCode < 48 || charCode > 57));
+      });
+      $('#end-line').keypress(function(event) {
+        var charCode = event.keyCode;
+        return !(charCode > 31 && (charCode < 48 || charCode > 57));
+      });
+      $('#start-line').on('input', triggerPartial);
+      $('#end-line').on('input', triggerPartial);
     }
 
     setFileName();
